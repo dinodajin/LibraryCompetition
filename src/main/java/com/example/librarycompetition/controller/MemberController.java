b00968bf79bd2d8734ffd468278a9dbce50669d8
kimgusxo <gusxoas9906@naver.com>
2024-08-22 12:11:41 +0900

    complete swagger docs

--- src/main/java/com/example/librarycompetition/controller/MemberController.java
+++ src/main/java/com/example/librarycompetition/controller/MemberController.java
@@ -1,11 +1,16 @@
 package com.example.librarycompetition.controller;
 
 import com.example.librarycompetition.dto.MemberDTO;
+import com.example.librarycompetition.exception.ErrorDTO;
 import com.example.librarycompetition.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "member", description = "Member API")
 @Slf4j
 @RestController
 @RequestMapping("/member")
@@ -24,8 +30,8 @@ public class MemberController {
 
     @Operation(summary = "Get Member", description = "멤버 인덱스로 멤버 한명 검색하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "멤버 검색 성공"),
-            @ApiResponse(responseCode = "404", description = "멤버가 존재하지 않습니다."),
+            @ApiResponse(responseCode = "200", description = "멤버 검색 성공", content = @Content(schema = @Schema(implementation = MemberDTO.class))),
+            @ApiResponse(responseCode = "404", description = "멤버가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @GetMapping("/get/{memberId}")
     public ResponseEntity<MemberDTO> getOneMember(@Parameter(description = "멤버 인덱스")
@@ -36,8 +42,8 @@ public class MemberController {
 
     @Operation(summary = "Get All Members", description = "모든 멤버 검색하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "멤버 리스트 검색 성공"),
-            @ApiResponse(responseCode = "404", description = "멤버 리스트가 존재하지 않습니다."),
+            @ApiResponse(responseCode = "200", description = "멤버 리스트 검색 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = MemberDTO.class)))),
+            @ApiResponse(responseCode = "404", description = "멤버 리스트가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @GetMapping("/get/all")
     public ResponseEntity<List<MemberDTO>> getAllMembers() {
@@ -47,8 +53,8 @@ public class MemberController {
 
     @Operation(summary = "Get Members", description = "멤버 이름으로 멤버 리스트 검색하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "멤버 리스트 검색 성공"),
-            @ApiResponse(responseCode = "404", description = "멤버 리스트가 존재하지 않습니다."),
+            @ApiResponse(responseCode = "200", description = "멤버 리스트 검색 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = MemberDTO.class)))),
+            @ApiResponse(responseCode = "404", description = "멤버 리스트가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @GetMapping("/get/memberName/{memberName}")
     public ResponseEntity<List<MemberDTO>> getMembersByMemberName(@Parameter(description = "멤버 이름")
@@ -59,8 +65,8 @@ public class MemberController {
 
     @Operation(summary = "Create Member", description = "멤버 정보로 멤버 생성하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "멤버 생성 성공"),
-            @ApiResponse(responseCode = "400", description = "멤버를 생성할 수 없습니다."),
+            @ApiResponse(responseCode = "201", description = "멤버 생성 성공", content = @Content(schema = @Schema(implementation = MemberDTO.class))),
+            @ApiResponse(responseCode = "400", description = "멤버를 생성할 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @PostMapping("/create")
     public ResponseEntity<MemberDTO> createMember(@Parameter(description = "생성할 멤버 정보를 담은 멤버 DTO")
@@ -71,8 +77,8 @@ public class MemberController {
 
     @Operation(summary = "Update Member", description = "멤버 정보로 멤버 수정하기")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "202", description = "멤버 수정 성공"),
-            @ApiResponse(responseCode = "400", description = "멤버를 수정할 수 없습니다."),
+            @ApiResponse(responseCode = "202", description = "멤버 수정 성공", content = @Content(schema = @Schema(implementation = MemberDTO.class))),
+            @ApiResponse(responseCode = "400", description = "멤버를 수정할 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @PutMapping("/update")
     public ResponseEntity<MemberDTO> updateMember(@Parameter(description = "수정할 멤버 정보를 담은 멤버 DTO")
@@ -84,7 +90,7 @@ public class MemberController {
     @Operation(summary = "Update Member", description = "멤버 인덱스로 멤버 삭제하기")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "204", description = "멤버 삭제 성공"),
-            @ApiResponse(responseCode = "400", description = "멤버를 삭제할 수 없습니다."),
+            @ApiResponse(responseCode = "400", description = "멤버를 삭제할 수 없습니다.", content = @Content(schema = @Schema(implementation = ErrorDTO.class))),
     })
     @DeleteMapping("/delete/{memberId}")
     public ResponseEntity<Void> deleteMember(@Parameter(description = "멤버 인덱스")
