442937e280420f54f7ce86858f0d9ea49f2394d3
dajin.jeong <djbrachio@gmail.com>
2024-08-22 10:29:35 +0900

    [MemberController]Set up

--- src/test/java/com/example/librarycompetition/unit/controller/MemberControllerUnitTest.java
+++ src/test/java/com/example/librarycompetition/unit/controller/MemberControllerUnitTest.java
@@ -1,4 +1,51 @@
 package com.example.librarycompetition.unit.controller;
 
+import com.example.librarycompetition.controller.MemberController;
+import com.example.librarycompetition.dto.MemberDTO;
+import com.example.librarycompetition.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.doNothing;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class MemberControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MemberService memberService;
+
+    private MemberDTO memberDTO;
+
+    @BeforeEach
+    @DisplayName("스텁 설정")
+    void setUp() {
+        // 테스트에 사용할 MemberDTO 객체를 초기화합니다.
+        String memberId = "memberTest";
+        String memberName = "John Doe";
+
+        memberDTO = MemberDTO.of(memberId, memberName);
+    }
 }
