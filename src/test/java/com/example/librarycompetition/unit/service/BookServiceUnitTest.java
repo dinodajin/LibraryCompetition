3efe14804879ca002d4fea965c539cc78394309a
dajin.jeong <djbrachio@gmail.com>
2024-08-22 10:45:06 +0900

    [BookService]Set up

--- src/test/java/com/example/librarycompetition/unit/service/BookServiceUnitTest.java
+++ src/test/java/com/example/librarycompetition/unit/service/BookServiceUnitTest.java
@@ -1,4 +1,50 @@
 package com.example.librarycompetition.unit.service;
 
+import com.example.librarycompetition.domain.Book;
+import com.example.librarycompetition.dto.BookDTO;
+import com.example.librarycompetition.exception.ListNotFoundElementException;
+import com.example.librarycompetition.exception.ResourceNotFoundException;
+import com.example.librarycompetition.repository.BookRepository;
+import com.example.librarycompetition.service.BookService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
 public class BookServiceUnitTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    private BookDTO bookDTO;
+    private Book book;
+
+    @BeforeEach
+    @DisplayName("스텁 설정")
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        String bookId = "testBookId";
+        Integer bookSequence = 1;
+        String bookTitle = "testTitle";
+        String bookAuthor = "testAuthor";
+        Integer bookDamage = 5;
+        String bookLabel = "testLabel";
+
+        bookDTO = BookDTO.of(bookId, bookSequence, bookTitle, bookAuthor, bookDamage, bookLabel);
+        book = bookDTO.toEntity();
+    }
 }
