adaefeea764845de584f1a040591e21c6985f131
dajin.jeong <djbrachio@gmail.com>
2024-08-22 11:03:19 +0900

    [memberService]Set up

--- src/test/java/com/example/librarycompetition/unit/service/MemberServiceUnitTest.java
+++ src/test/java/com/example/librarycompetition/unit/service/MemberServiceUnitTest.java
@@ -1,4 +1,46 @@
 package com.example.librarycompetition.unit.service;
 
+import com.example.librarycompetition.domain.Member;
+import com.example.librarycompetition.dto.MemberDTO;
+import com.example.librarycompetition.exception.ListNotFoundElementException;
+import com.example.librarycompetition.exception.ResourceNotFoundException;
+import com.example.librarycompetition.repository.MemberRepository;
+import com.example.librarycompetition.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
 public class MemberServiceUnitTest {
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @InjectMocks
+    private MemberService memberService;
+
+    private MemberDTO memberDTO;
+    private Member member;
+
+    @BeforeEach
+    @DisplayName("스텁 설정")
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        String memberId = "testMemberId";
+        String memberName = "testMemberName";
+
+        memberDTO = MemberDTO.of(memberId, memberName);
+        member = memberDTO.toEntity();
+    }
 }
