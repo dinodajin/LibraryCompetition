185000e93ae48d1a12763f787cc3e06408516281
dajin.jeong <djbrachio@gmail.com>
2024-08-22 10:05:45 +0900

    Set up ImageControllerUnitTest

--- src/test/java/com/example/librarycompetition/unit/controller/ImageControllerUnitTest.java
+++ src/test/java/com/example/librarycompetition/unit/controller/ImageControllerUnitTest.java
@@ -1,4 +1,54 @@
 package com.example.librarycompetition.unit.controller;
 
+import com.example.librarycompetition.controller.ImageController;
+import com.example.librarycompetition.dto.ImageDTO;
+import com.example.librarycompetition.service.ImageService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.doNothing;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ImageController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ImageControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ImageService imageService;
+
+    private ImageDTO imageDTO;
+
+    @BeforeEach
+    @DisplayName("스텁 설정")
+    void setUp() {
+        // 테스트에 사용할 ImageDTO 객체를 초기화합니다.
+        String imageId = "test";
+        String bookId = "bookTest";
+        Integer cameraId = 123;
+        LocalDate imageTime = LocalDate.of(2023, 8, 21);
+
+        imageDTO = ImageDTO.of(imageId, bookId, cameraId, imageTime);
+    }
 }
